529917a04fc61271dd8e11da5d7677c9ff30a148
Riza Erdi Karakus <karakuserdi@gmail.com>
2021-08-13 10:52:10 +0300

    Initial Commit

---
+++ FitnessHubApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FitnessHubApp
+//
+//  Created by Riza Erdi Karakus on 13.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
